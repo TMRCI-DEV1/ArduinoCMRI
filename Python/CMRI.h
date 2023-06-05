67c56ccc46acaa4a4b189caf7bcc9c0dc88a4251
The Model Railroad Club, Inc <thomas.seitz@tmrci.org>
2023-06-05 15:35:11 -0400

    Update CMRI.h

--- Python/CMRI.h
+++ Python/CMRI.h
@@ -30,25 +30,134 @@ class CMRI:
         self._address = address
 
     def process(self):
-        pass  # Implement this method
+        while self._serial.in_waiting > 0:
+            if self.process_char(self._serial.read()):
+                return True
+        return False
 
     def process_char(self, c):
-        pass  # Implement this method
+        switcher = {
+            POLL: self.transmit,
+            SET: lambda: True
+        }
+        return switcher.get(self._decode(c), lambda: False)()
 
-    def transmit(self):
-        pass  # Implement this method
+    def get_bit(self, pos):
+        c = self.get_byte(pos // 8)
+        return (bool) ((c >> (pos % 8)) & 0x01)
 
-    def get_bit(self, n):
-        pass  # Implement this method
+    def get_byte(self, pos):
+        if pos > self._rx_length:
+            return 0  # out of bounds
+        else:
+            return self._rx_buffer[pos]
 
-    def get_byte(self, n):
-        pass  # Implement this method
+    def set_bit(self, pos, bit):
+        if ((pos + 7) // 8 > self._tx_length):
+            return False  # out of bounds
+        else:
+            index = pos // 8
+            self._tx_buffer[index] = bit if bit else self._tx_buffer[index] & ~(1 << pos % 8)
+            return True
 
-    def set_bit(self, n, b):
-        pass  # Implement this method
+    def set_byte(self, pos, b):
+        if pos > self._tx_length:
+            return False  # out of bounds
+        else:
+            self._tx_buffer[pos] = b
+            return True
 
-    def set_byte(self, n, b):
-        pass  # Implement this method
+        def transmit(self):
+        self._serial.write(bytes([255, 255, STX, 65 + self._address, GET]))
+        for i in range(self._tx_length):
+            if self._tx_buffer[i] == ETX or self._tx_buffer[i] == ESC:
+                self._serial.write(bytes([ESC]))  # escape because this looks like an STX bit or an escape bit (very basic protocol)
+            self._serial.write(bytes([self._tx_buffer[i]]))
+        self._serial.write(bytes([ETX]))
 
     def _decode(self, c):
-        pass  # Implement this method
+        switcher = {
+            PREAMBLE_1: self._preamble_1,
+            PREAMBLE_2: self._preamble_2,
+            PREAMBLE_3: self._preamble_3,
+            DECODE_ADDR: self._decode_addr,
+            DECODE_CMD: self._decode_cmd,
+            IGNORE_CMD: self._ignore_cmd,
+            DECODE_DATA: self._decode_data,
+            IGNORE_DATA: self._ignore_data,
+            DECODE_ESC_DATA: self._decode_esc_data,
+            IGNORE_ESC_DATA: self._ignore_esc_data,
+            POSTAMBLE_OTHER: self._postamble_other,
+            NOOP: self._noop
+        }
+        return switcher.get(self._mode, self._noop)(c)
+
+    def _preamble_1(self, c):
+        self._rx_index = 0
+        if c == 0xFF:
+            self._mode = PREAMBLE_2
+
+    def _preamble_2(self, c):
+        if c == 0xFF:
+            self._mode = PREAMBLE_3
+        else:
+            self._mode = PREAMBLE_1
+
+    def _preamble_3(self, c):
+        if c == STX:
+            self._mode = DECODE_ADDR
+        else:
+            self._mode = PREAMBLE_1
+
+    def _decode_addr(self, c):
+        if c == 'A' + self._address:
+            self._mode = DECODE_CMD
+        elif c >= 'A':
+            self._mode = IGNORE_CMD
+        else:
+            self._mode = PREAMBLE_1
+
+    def _decode_cmd(self, c):
+        if c == SET:
+            self._mode = DECODE_DATA
+        elif c == POLL:
+            self._mode = POSTAMBLE_POLL
+        else:
+            self._mode = POSTAMBLE_OTHER
+
+    def _ignore_cmd(self, c):
+        self._mode = IGNORE_DATA
+
+    def _decode_data(self, c):
+        if c == ESC:
+            self._mode = DECODE_ESC_DATA
+        elif c == ETX:
+            self._mode = POSTAMBLE_SET
+        elif self._rx_index >= self._rx_length:
+            pass
+        else:
+            self._rx_buffer[self._rx_index] = c
+            self._rx_index += 1
+
+    def _decode_esc_data(self, c):
+        if self._rx_index >= self._rx_length:
+            pass
+        else:
+            self._rx_buffer[self._rx_index] = c
+            self._rx_index += 1
+        self._mode = DECODE_DATA
+
+    def _ignore_data(self, c):
+        if c == ESC:
+            self._mode = IGNORE_ESC_DATA
+        elif c == ETX:
+            self._mode = POSTAMBLE_IGNORE
+
+    def _ignore_esc_data(self, c):
+        self._mode = IGNORE_DATA
+
+    def _postamble_other(self, c):
+        self._mode = PREAMBLE_1
+
+    def _noop(self, c):
+        pass
